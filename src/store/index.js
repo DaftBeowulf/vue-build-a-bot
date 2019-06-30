457649ab34a522eaf9be3c06d90cd2fdda43cf46
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-06-30 17:20:41 -0500

    :sparkles: Getting all data from API
    Proxy set up to listen on API local port, all data now being retrieved and referenced via mixin for parts, deleted data folder

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     cart: [],
+    parts: null,
   },
   mutations: {
     addRobotToCart(state, robot) {
       state.cart.push(robot);
+    },
+    updateParts(state, parts) {
+      state.parts = parts;
+    },
+  },
+  actions: {
+    getParts({ commit }) {
+      axios
+        .get('/api/parts')
+        .then(res => commit('updateParts', res.data))
+        .catch(console.error);
     },
   },
   getters: {
